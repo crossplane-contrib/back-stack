f876c48b3937aca2b69e5f46a640ab619b660218
Murph <murph@opusdavei.org>
2023-10-26 21:05:44 +0100

    update local install

--- local-install.sh
+++ local-install.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
 # set up kind cluster
-kind create cluster --name backstack --wait 5m
+kind create cluster --name backstack --wait 5m --config=- <<- EOF
+  kind: Cluster
+  apiVersion: kind.x-k8s.io/v1alpha4
+  nodes:
+  - role: control-plane
+    kubeadmConfigPatches:
+    - |
+      kind: InitConfiguration
+      nodeRegistration:
+        kubeletExtraArgs:
+          node-labels: "ingress-ready=true"        
+    extraPortMappings:
+    - containerPort: 80
+      hostPort: 80
+      protocol: TCP
+    - containerPort: 443
+      hostPort: 443
+      protocol: TCP
+EOF
+
+# configure ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.1/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s 
 
 # install crossplane
 helm install crossplane --namespace crossplane-system --create-namespace crossplane-stable/crossplane --wait
@@ -28,10 +50,31 @@ kubectl create -f - <<- EOF
         source: InjectedIdentity
 EOF
 
+# configure provider-kubernetes for crossplane
+kubectl create -f - <<- EOF
+    apiVersion: pkg.crossplane.io/v1
+    kind: Provider
+    metadata:
+      name: provider-kubernetes
+    spec:
+      package: xpkg.upbound.io/crossplane-contrib/provider-kubernetes:v0.9.0
+EOF
+kubectl wait provider/provider-kubernetes --for=condition=Healthy --timeout=1m
+SA=$(kubectl -n crossplane-system get sa -o name | grep provider-kubernetes | sed -e 's|serviceaccount\/|crossplane-system:|g')
+kubectl create clusterrolebinding provider-kubernetes-admin-binding --clusterrole cluster-admin --serviceaccount="${SA}"
+kubectl create -f - <<- EOF
+    apiVersion: kubernetes.crossplane.io/v1alpha1
+    kind: ProviderConfig
+    metadata:
+      name: local
+    spec:
+      credentials:
+        source: InjectedIdentity
+EOF
+
 # install hub composition
 kubectl apply -f crossplane/apis/hub
+kubectl wait crd/hubs.backstack.cncf.io --for=condition=Established --timeout=1m
 
 # deploy hub
 kubectl apply -f crossplane/examples/hub.yaml
-
-
